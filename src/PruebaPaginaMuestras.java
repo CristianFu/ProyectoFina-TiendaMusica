30a3422c490f069a0e0ee64c5c3ecd7022695d64
CristianFu <134558440+CristianFu@users.noreply.github.com>
2023-11-27 15:14:32 -0600

    HTML Prueba

---
+++ src/PruebaPaginaMuestras.java
@@ -0,0 +1,7 @@
+public class PruebaPaginaMuestras {
+    public static void main(String[] args) {
+    MuestrasHTML pagina=new
+                MuestrasHTML ("CancionesMuestra.html");
+        pagina.crearPagina();
+    }
+}
